ff8b402aeeca967159b3addd3525d4ca67591cc8
Vasiok11 <pascari.vasile27@gmail.com>
2026-03-15 19:34:44 +0200

    Set up eleventy

---
+++ landing-page-lab3/.eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+  // Pass through css and images directly to the output folder
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["html", "njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
